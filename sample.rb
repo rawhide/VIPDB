220850844f41feeaa70f2586b1faf0a3df0efffc
ohta <ohta@raw-hide.co.jp>
2013-11-30 16:18:32 +0900

    sample.rb

--- sample.rb
+++ sample.rb
@@ -3,8 +3,17 @@ require 'nokogiri'
  
 board = open("http://hayabusa.2ch.net/test/read.cgi/news4vip/1385794291").read
 doc = Nokogiri::HTML.parse(board)
-doc.css('//dl[@class="thread"]').each do |thread|
-  thread.css("dt").each do |res|
-    p res.text.split("：")
+
+
+doc.css('//dl[@class="thread"]').each_with_index do |thread, i|
+  res_headers = thread.css("dt")
+  res_bodies = thread.css("dd")
+
+  [].tap do |result|
+    res_headers.each do |res_head|
+      result.push({ title: res_headers[i], body: res_bodies[i] })
+    end
+    p result
   end
+  
 end
