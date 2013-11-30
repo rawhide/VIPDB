c75cd8f80a5b1b3afa513a090c8e572c58e03143
坂本 <sakamoto@MacBook-Air.local>
2013-11-30 14:39:16 +0900

    db commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+VIPDB::Application.config.secret_key_base = '098184dec74a5cc27f488278de87e7ab4640af56164a7a98ba1c4a41bd85a44747d3bc8776c47292ab8f2ea5a5f916dd4050f56e11e5146af84cd9976cdec871'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+VIPDB::Application.config.session_store :cookie_store, key: '_VIPDB_session'
