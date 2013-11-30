395a78a2210ebf493c587f5f65a447f1d3c78330
坂本 <sakamoto@MacBook-Air.local>
2013-11-30 15:23:34 +0900

    db/seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,9 +6,8 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 [
-  {title: "test_1", url: ""},
-  {title: "test_2", url: ""},
-  {title: "test_3", url: ""},
+  {title: "test_1", url: "http://hayabusa.2ch.net/news4vip/"},
+  {title: "test_2", url: "http://hayabusa3.2ch.net/mnewsplus/"}
 ].each do |scraping|
   s = Scraping.new(scraping)
 
@@ -20,12 +19,14 @@
     {name: "スレッド_3"},
     {name: "スレッド_4"},
   ].each do |board|
-    b = s.build_board(board)
+    b = s.boards.build(board)
     next unless b.save
     t = Time.now
     [
       {vaice_body: "内容", voice_name: "投稿者1", posted_at: t.to_s, voice_id: ["hoge", "fuga", "moge", "koge"][rand(4)]}
-    ]
-    c = b.build_comment
+    ].each do |comment|
+      c = b.comments.build(comment)
+      p c.save
+    end
   end
 end
